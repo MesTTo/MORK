5b7f434cc408c0cf6ffe44cdcbf66a280c886cbe
MesTTo <a.mesto@student.unsw.edu.au>
2026-06-23 17:10:44 +1000

    Fix exported prefix macro

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -8,3 +8,21 @@ pub mod space;
 mod sources;
 mod sinks;
 mod pure;
+
+#[doc(hidden)]
+pub use mork_expr as __mork_expr;
+#[doc(hidden)]
+pub use mork_frontend as __mork_frontend;
+
+pub mod prefix {
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub struct Prefix<'a> {
+        pub slice: &'a [u8],
+    }
+
+    impl<'a> Prefix<'a> {
+        pub fn path(&self) -> &'a [u8] {
+            self.slice
+        }
+    }
+}
